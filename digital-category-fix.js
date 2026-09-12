9b694465cd6e095f97d4fc0e830bb006f653d82d
27andriansyah <duatujuhandriansyah@gmail.com>
2026-09-13 03:47:41 +0700

    Perbaiki kategori dan metadata produk digital

--- digital-category-fix.js
+++ digital-category-fix.js
@@ -8,6 +8,7 @@ const DIGITAL_CATEGORY_MAP={
   ppob:new Set(Array.from({length:15},(_,i)=>220+i))
 };
 const DIGITAL_CAT_IDS=new Set([9,10,11,12,13,14,15,16,17,18,...Array.from({length:119},(_,i)=>116+i)]);
+for(const cat of ['game','voucher','hiburan','ppob']) DIGITAL.add(cat);
 PRODUCTS.forEach(p=>{
   for(const [cat,ids] of Object.entries(DIGITAL_CATEGORY_MAP)) if(ids.has(p.id)) p.cat=cat;
 });
